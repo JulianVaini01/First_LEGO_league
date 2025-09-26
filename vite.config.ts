54f642270b6a82b10347a6ebcb263cb0ce6552f1
JulianVaini01 <vainillajuliandavid@gmail.com>
2025-09-26 15:23:29 -0500

    Configure project for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/First_LEGO_league/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

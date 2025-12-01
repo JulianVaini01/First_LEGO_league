13a86b6f4a08bb44f190443d1423127d6ce9af33
JulianVaini01 <vainillajuliandavid@gmail.com>
2025-12-01 16:13:46 -0500

    Updated ScoreDisplayPage.tsx

--- src/components/ScoreDisplayPage.tsx
+++ src/components/ScoreDisplayPage.tsx
@@ -65,9 +65,9 @@ export default function ScoreDisplayPage({ scores, onNavigate }: ScoreDisplayPag
                 className="h-8 w-auto md:h-10"
               />
               <img 
-                src="./Imagen1.jpg" 
+                src="./Imagen1.png" 
                 alt="UNNO" 
-                className="h-8 w-auto md:h-10 bg-white rounded-lg px-1 py-0.5 md:px-2 md:py-1"
+                className="h-8 w-auto md:h-10 rounded-lg px-1 py-0.5 md:px-2 md:py-1"
               />
             </div>
             {/* Mobile logos */}

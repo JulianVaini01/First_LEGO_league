9550fc9e06fca465aec1a7244aa874bf51a80e8d
JulianVaini01 <vainillajuliandavid@gmail.com>
2025-12-01 11:13:52 -0500

    Updated HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -19,7 +19,7 @@ export default function HomePage({ onNavigate }: HomePageProps) {
               className="h-8 w-auto md:h-12"
             />
             <img 
-              src="./Imagen1.jpg" 
+              src="./Imagen1.png" 
               alt="UNNO" 
               className="h-8 w-auto md:h-12 bg-white rounded-lg px-1 py-0.5 md:px-2 md:py-1"
             />

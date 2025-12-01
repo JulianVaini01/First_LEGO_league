43124a36bdb21144d2114c05cf4ff03c900e1737
JulianVaini01 <vainillajuliandavid@gmail.com>
2025-12-01 11:39:37 -0500

    Updated HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -16,7 +16,7 @@ export default function HomePage({ onNavigate }: HomePageProps) {
             <img 
               src="./Imagen2.png" 
               alt="First Lego League" 
-              className="h-8 w-auto md:h-12"
+              className="h-8 w-auto md:h-15"
             />
             <img 
               src="./Imagen1.png" 

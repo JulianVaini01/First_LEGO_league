522d8038ec8ca11d8278a0b608d7617deb1d6460
JulianVaini01 <vainillajuliandavid@gmail.com>
2025-12-03 12:16:21 -0500

    Updated ScoringPage.tsx

--- src/components/ScoringPage.tsx
+++ src/components/ScoringPage.tsx
@@ -271,12 +271,12 @@ export default function ScoringPage({ onNavigate, onAddScore }: ScoringPageProps
             <img 
               src="./Imagen2.png" 
               alt="First Lego League" 
-              className="h-20 w-auto md:h-10"
+              className="h-20 w-auto md:h-20"
             />
             <img 
               src="./Imagen1.png" 
               alt="UNNO" 
-              className="h-20 w-auto md:h-10 rounded-lg px-1 py-0.5 md:px-2 md:py-1"
+              className="h-20 w-auto md:h-20 rounded-lg px-1 py-0.5 md:px-2 md:py-1"
             />
             {/* Mobile logos */}
             <div className="md:hidden absolute top-4 right-4 flex items-center space-x-2">

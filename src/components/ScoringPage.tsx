cd655927dc568ca6e7ad38d8eeda6159f1006534
JulianVaini01 <vainillajuliandavid@gmail.com>
2026-02-09 10:04:04 -0500

    Updated ScoringPage.tsx

--- src/components/ScoringPage.tsx
+++ src/components/ScoringPage.tsx
@@ -362,7 +362,7 @@ export default function ScoringPage({ onNavigate, onAddScore }: ScoringPageProps
                 <img
                   src={noEquipmentImg}
                   alt="No equipamiento"
-                  className="h-16,2 w-16,2 object-contain"
+                  className="h-16 w-16 object-contain"
                 />
               </div>
               <div className="flex-1">
